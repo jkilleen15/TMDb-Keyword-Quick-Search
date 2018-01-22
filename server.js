ce4fd885303aea0308da66c78e6e2a9a8a84fe0a
Mac User <macuser@macs-mbp.gateway.pace.com>
2018-01-21 20:21:20 -0800

    revisions for heroku, in progress

--- server.js
+++ server.js
@@ -1,13 +1,33 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+// import oilRoutes from './routes/OilRoutes';
+// import mongoose from 'mongoose';
+
+// mongoose.set('debug', true);
+// mongoose.Promise = global.Promise;
+// mongoose.connect('mongodb://localhost/oilAppDB');
+
+const myMovieServer = express();
+myMovieServer.use(bodyParser.json());
+// myMovieServer.use(oilRoutes);
+
+const port = process.env.PORT || 8080;
+myMovieServer.listen(port, () => {
+  console.log(`Listening on port:${port}`);
+});
+
+/*
+const path = require('path');
 const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/'));
+app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(__dirname + '/'));
 app.listen(port);
 
 console.log('server started');
-
-
+*/
 
 /*
 const path = require('path');
