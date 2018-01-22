49eed2b6dc9cb2bda9ee63619ad7fc2cc5c97cb5
Mac User <macuser@macs-mbp.gateway.pace.com>
2018-01-21 18:48:05 -0800

    adjust server.js for heroku

--- server.js
+++ server.js
@@ -3,9 +3,23 @@ const express = require('express');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, 'dist')));
+// app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(__dirname + '/'));
 app.set('port', process.env.PORT || 8080);
+
+app.get('*', (req, res) =>{
+  // res.sendFile(path.resolve(__dirname, 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'script.jsx'));
+});
 
 const server = app.listen(app.get('port'), function() {
   console.log('listening on port ', server.address().port);
 });
+
+// app.use(express.static(__dirname + '/'));
+// ////////////////////
+// app.get('*', (req, res) =>{
+  // res.sendFile(path.resolve(__dirname, 'index.html'));
+// });
+// ////////////////////
+// not used // app.listen(port);
